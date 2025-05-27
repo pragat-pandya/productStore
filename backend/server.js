459219291c232c19ebeadacde892b74ba17da6f9
Pragat Pandya <pragat.pandya@gmail.com>
2025-05-28 02:50:38 +0530

    Add delete product API.

--- backend/server.js
+++ backend/server.js
@@ -32,6 +32,26 @@ app.post("/api/products", async (req, res) => {
     }
 });
 
+
+app.delete("/api/products/:id", async (req, res) => {
+    const {id} = req.params;
+    console.log("id:", id);
+
+    try {
+        await Product.findByIdAndDelete(id);
+        res.status(200).json({
+            success: true,
+            message: "Product Deleted"
+        });
+    } catch (error) {
+        console.log("Error in deleting the product", error.message);
+        res.status(404).json({
+            success: false,
+            message: "Product Not Found"
+        });
+    }
+});
+
 app.listen(5000, () => {
     connectDb();
     console.log("Server Started at URL: http://localhost:5000")
