a810fa23fd2553a93a3ddc2dbbf1d9f9492d45c7
Pragat Pandya <pragat.pandya@gmail.com>
2025-05-28 02:06:43 +0530

    Connect to database while start listening on 5000 port in server.

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,12 @@
 import express from 'express';
-
+import { connectDb } from './config/db.js';
 const app = express();
 
 app.get("/", (req, res) => {
     res.send("Server is ready!");
 })
 
-
 app.listen(5000, () => {
+    connectDb();
     console.log("Server Started at URL: http://localhost:5000")
 });
-
-// QP9dU9tuNmvKRNRU
